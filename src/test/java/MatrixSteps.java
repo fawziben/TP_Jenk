7ee9e6071cd902ff1fff3e2a1a04a814dd703df8
hp <if_benmoumen@esi.dz>
2023-02-08 14:51:23 +0100

    modify jenkins file

--- src/test/java/MatrixSteps.java
+++ src/test/java/MatrixSteps.java
@@ -42,7 +42,6 @@ public class MatrixSteps {
     }
 
 
-
     @Then("The result of determinant is {}")
     public void iFindAsDeterminantResult(double arg0) {
         Assert.assertEquals(arg0,det,0);
